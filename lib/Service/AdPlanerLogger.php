da88dc1d92d4c8be7aec7b94ed84eda51dc57bc1
filzmann <simonbeyer79@gmail.com>
2026-07-04 15:51:05 +0200

    Use LocalBase app logger

--- lib/Service/AdPlanerLogger.php
+++ lib/Service/AdPlanerLogger.php
@@ -5,35 +5,16 @@ declare(strict_types=1);
 namespace OCA\AdPlaner\Service;
 
 use OCA\AdPlaner\AppInfo\Application;
-use OCP\IUserSession;
-use Psr\Log\LoggerInterface;
+use OCA\LocalBase\Service\AppLogger;
 use Throwable;
 
 class AdPlanerLogger {
     public function __construct(
-        private LoggerInterface $logger,
-        private IUserSession $userSession
+        private AppLogger $logger
     ) {
     }
 
     public function error(string $action, Throwable $exception, array $context = []): void {
-        $safeContext = [];
-        foreach ($context as $key => $value) {
-            if (is_scalar($value) || $value === null) {
-                $safeContext[(string)$key] = $value;
-            }
-        }
-
-        $safeContext['app'] = Application::APP_ID;
-        $safeContext['action'] = $action;
-        $safeContext['exception_class'] = get_class($exception);
-        $safeContext['exception_message'] = $exception->getMessage();
-
-        $user = $this->userSession->getUser();
-        if ($user !== null) {
-            $safeContext['user_id'] = $user->getUID();
-        }
-
-        $this->logger->error('AdPlaner error during ' . $action, $safeContext);
+        $this->logger->error(Application::APP_ID, 'AdPlaner', $action, $exception, $context);
     }
 }
